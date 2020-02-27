2737f4ad6434125199d6492c3cb99d0ed27f3d93
Daniel Ruaro <danielruaro@hotmail.com>
2020-02-27 19:05:54 -0300

    fix url

--- src/services/ImdbServices.js
+++ src/services/ImdbServices.js
@@ -1,38 +1,40 @@
-import axios from 'axios'
+import axios from "axios";
 
-const url = 'http://localhost:3001/'
+const url = process.env.REACT_APP_API_URL || `https://localhost:3001/`;
 
 class ImdbServices {
+  async getMovies(loadCount) {
+    return await axios
+      .get(url + `movies?page=${loadCount}`)
+      .then(function({ data }) {
+        return data.results;
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
 
-    async getMovies(loadCount) {
-        return await axios.get(url + `movies?page=${loadCount}`)
-            .then(function ({ data }) {
-                return data.results
-            })
-            .catch(function (error) {
-                return error
-            })
-    }
+  async searchMovies(param) {
+    return await axios
+      .get(url + `search?movie=${param}`)
+      .then(function({ data }) {
+        return data.results;
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
 
-    async searchMovies(param) {
-        return await axios.get(url + `search?movie=${param}`)
-            .then(function ({ data }) {
-                return data.results
-            })
-            .catch(function (error) {
-                return error
-            })
-    }
-
-    async getGenre(param) {
-        return await axios.get(url + `genre/${param}`)
-            .then(function ({ data }) {
-                return data.genre
-            })
-            .catch(function (error) {
-                return error
-            })
-    }
+  async getGenre(param) {
+    return await axios
+      .get(url + `genre/${param}`)
+      .then(function({ data }) {
+        return data.genre;
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
 }
 
-export default new ImdbServices()
+export default new ImdbServices();
