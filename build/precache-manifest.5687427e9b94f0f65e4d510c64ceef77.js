7073d9296317f79a2fd85c0450aabe31083c9a7e
Daniel Ruaro <daniel.prestes@tekoa.com.br>
2020-02-27 17:01:21 -0300

    react-create-app on Heroku

---
+++ build/precache-manifest.5687427e9b94f0f65e4d510c64ceef77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69480562e444a18dd9c9d76bd83491de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cc53cb4a17d826c117e",
+    "url": "/static/css/main.b4c54558.chunk.css"
+  },
+  {
+    "revision": "830420d468f382fdef8f",
+    "url": "/static/js/2.6f48e458.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6f48e458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc53cb4a17d826c117e",
+    "url": "/static/js/main.94809133.chunk.js"
+  },
+  {
+    "revision": "d9caded81823a01eb4d2",
+    "url": "/static/js/runtime-main.c2d99466.js"
+  }
+]);
